107b249e6e69a3652aa6cd31964b274e2436e576
Nathan Tran <nathancongtran@gmail.com>
2025-04-03 22:26:35 -0700

    .csv .venv and .gitignore
    .csv file from Kaggle, created .venv and pip libraries needed for ML.  Created .gitignore and placed .venv environment.

---
+++ main.py
@@ -0,0 +1,11 @@
+#For Numerical Operations and Data Manipulations
+import numpy as np
+import pandas as pd
+
+#For building and training deep learning models
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+#For Data Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns

0ccbca4bf067a00fd5a11b571c7e89c1e3477bfb
Nathan Tran <nathancongtran@gmail.com>
2025-04-05 00:56:50 -0700

    update main.py
    managed to replace the NaN values with the mean of the data, then filtered the data to only include numerics. Then normalized the data using a MinMaxScaler.

--- main.py
+++ main.py
@@ -15,32 +15,32 @@ import seaborn as sns
 #References:
     #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
     #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.fillna.html
+    #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.filter.html
+    #https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.MinMaxScaler.html#sklearn.preprocessing.MinMaxScaler
 
+#Load the Data
 data = pd.read_csv("internet_usage.csv")
 
+#get the columns that are years in ranges 2000, 2024
 years_cols = [str(years) for years in range(2000, 2024)]
 
+#convert the data in those columns to numeric
 for col in years_cols:
     data[col] = pd.to_numeric(data[col], errors = "coerce")
 
-#for col in years_cols:
-  #  print(data[col].unique())
-
-#print(data[years_cols].mean())
-
-print(data[years_cols].isna().sum())
-
-data[years_cols].fillna(data[years_cols].mean(), inplace=True)
-
-print(data[years_cols].isna().sum())
-
-
+#get the mean of the data and replace the NaN values with them
+data[years_cols] = data[years_cols].fillna(data[years_cols].mean())
 
 #2. Feature Selection and Preparation
+data_filtered = data.filter(items = years_cols)
 
 #3. Normalize the Data
+scaler = MinMaxScaler(feature_range=(0, 1))
+data_filtered_scaled = scaler.fit_transform(data_filtered)
 
 #4 Create Sequences for the LSTM
+
+
 
 #5 Split the Data into Training and Testing Sets
 
